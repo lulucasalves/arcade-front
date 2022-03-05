aa1b23242db0f7ccb4d96d098aa594bc9d1deda9
lulucas <lucas.alves.supus@outlook.com>
2022-03-05 13:58:06 -0300

    update tetris keys

--- tetris/scripts/app.js
+++ tetris/scripts/app.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const scoreDisplay = document.querySelector('#score')
   const startBtn = document.querySelector('#start-button')
-  const width = 10
+  const width = 22
 
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
     iTetromino
   ]
 
-  let currentPosition = 4
+  let currentPosition = 10
   let currentRotation = 0
 
   let random = Math.floor(Math.random() * theTetrominoes.length)
@@ -83,5 +83,83 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  function undraw() {}
+  function undraw() {
+    current.forEach((i) => {
+      squares[currentPosition + i].classList.remove('tetromino')
+    })
+  }
+
+  timerId = setInterval(moveDown, 1000)
+
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft()
+    } else if (e.keyCode === 38) {
+      rotate()
+    } else if (e.keyCode === 39) {
+      moveRight()
+    } else if (e.keyCode === 40) {
+      moveDown()
+    }
+  }
+
+  document.addEventListener('keyup', control)
+
+  function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+  }
+
+  function freeze() {
+    if (
+      current.some((i) =>
+        squares[currentPosition + i + width].classList.contains('taken')
+      )
+    ) {
+      random = Math.floor(Math.random() * theTetrominoes.length)
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+    }
+  }
+
+  function moveLeft() {
+    undraw()
+    const isAtLeft = current.some((i) => (currentPosition + i) % width === 0)
+    if (!isAtLeft) {
+      currentPosition--
+    }
+
+    if (
+      current.some((i) =>
+        squares[currentPosition + i].classList.contains('taken')
+      )
+    ) {
+      currentPosition++
+    }
+
+    draw()
+  }
+
+  function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(
+      (index) => (currentPosition + index) % width === width - 1
+    )
+    if (!isAtRightEdge) {
+      currentPosition++
+    }
+
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains('taken')
+      )
+    ) {
+      currentPosition--
+    }
+
+    draw()
+  }
 })
