35283ed5fdae32caa05904bd911259c582be127b
lulucas <lucas.alves.supus@outlook.com>
2022-03-15 15:24:11 -0300

    test

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const routers = require('./router')
+const favicon = require('serve-favicon')
+const path = require('path')
 require('dotenv/config')
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(cors())
 app.use(routers)
 
