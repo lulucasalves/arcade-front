1a1891fa63785608b9bdbad3944c9dee1815bf35
lulucasalves <lucas.alves.supus@outlook.com>
2022-03-21 15:37:22 -0300

    add current port

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.svg')))
 app.use(cors())
 app.use(routers)
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
